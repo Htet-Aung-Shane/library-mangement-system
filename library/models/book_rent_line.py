42fd1cf1066ca5c280d0f7f4e49223870622737c
Htet Aung Shane <mmc.htetaungshane@gmail.com>
2024-06-25 00:16:32 +0630

    update from local computer

--- library/models/book_rent_line.py
+++ library/models/book_rent_line.py
@@ -9,7 +9,7 @@ class BookRentLine(models.Model):
     onhand_quantity = fields.Integer("On Hand Quantity", compute="_compute_book_info")
     author_id = fields.Many2one('book.author', string="Author", compute="_compute_book_info")
     category_ids = fields.Many2many("book.category", string="Categories", compute="_compute_book_info")
-    @api.onchange('book_id')
+    @api.depends('book_id')
     def _compute_book_info(self):
         if self.book_id:
             self.onhand_quantity = self.book_id.onhand_qty
@@ -24,5 +24,25 @@ class BookRentLine(models.Model):
     rent_date = fields.Date('Rent Date', default=fields.Date.today())
     acceptance_date = fields.Date('Acceptance Date')
     expire_date = fields.Date('Expired Date')
-    is_penalty = fields.Boolean('Is Penalty')
+    is_penalty = fields.Boolean('Is Penalty', compute="_compute_is_penalty")
     rent_id = fields.Many2one('book.rent', string="Rent")
+    is_returned = fields.Boolean('Is Returned')
+
+    @api.depends('rent_date', 'expire_date')
+    def _compute_is_penalty(self):
+        for line in self:
+            if line.rent_date and line.expire_date:
+                if line.rent_date <= line.expire_date:
+                    line.is_penalty = False
+                else:
+                    line.is_penalty = True
+            else:
+                line.is_penalty = False
+
+    @api.depends('acceptance_date')            
+    def _compute_is_returned(self):
+        for line in self:
+            if line.acceptance_date:
+                line.is_returned = True
+            else:
+                line.is_returned = False
