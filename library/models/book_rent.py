71a0dbdd1aeca923a318f1400c43b9b6c9b40226
Htet-Aung-Shane <mmc.htetaungshane@gmail.com>
2024-06-25 15:01:13 +0630

    update library logic

--- library/models/book_rent.py
+++ library/models/book_rent.py
@@ -11,8 +11,8 @@ class BookRent(models.Model):
         default=lambda self:
         self.env['ir.sequence'].next_by_code('book.rent'))  
     rent_date = fields.Date('Rent Date', default=fields.Date.today())
-    author_ids = fields.Many2many('book.author', string="Authors")
-    category_ids = fields.Many2many("book.category")
+    author_ids = fields.Many2many('book.author', string="Authors", compute='_compute_author_ids', store=True)
+    category_ids = fields.Many2many("book.category", compute='_compute_category_ids', store=True)
     admin_id = fields.Many2one('res.partner', string="Admin")
     student_id = fields.Many2one('student', string="Student")
     rent_ids = fields.One2many(
@@ -26,3 +26,19 @@ class BookRent(models.Model):
     def create(self, vals):
         vals["admin_id"] = self.env.user.partner_id.id
         return super(BookRent, self).create(vals)
+    
+    @api.depends('rent_ids.book_id')
+    def _compute_author_ids(self):
+        for rec in self:
+            if rec.rent_ids:
+                rec.author_ids = rec.rent_ids.mapped('book_id.author_id')
+            else:
+                rec.author_ids = False
+    
+    @api.depends('rent_ids.book_id')
+    def _compute_category_ids(self):
+        for rec in self:
+            if rec.rent_ids:
+                rec.category_ids = rec.rent_ids.mapped('book_id.category_ids')
+            else:
+                rec.category_ids = False
